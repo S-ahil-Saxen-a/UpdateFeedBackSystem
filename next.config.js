318f53cf16375f10b7918429e44ec47950f47688
Sahil <sahilsaxena9450@gmail.com>
2023-08-14 00:11:24 +0530

    commenting images domains in next.config.js

--- next.config.js
+++ next.config.js
@@ -1,9 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
-  images: {
-    domains: ["oaidalleapiprodscus.blob.core.windows.net"],
-  },
+  // images: {
+  //   domains: ["oaidalleapiprodscus.blob.core.windows.net"],
+  // },
 };
 
 module.exports = nextConfig;
